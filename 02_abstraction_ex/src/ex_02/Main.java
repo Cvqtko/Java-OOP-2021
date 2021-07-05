d5b54c3916e75991b6f710100358fc5dd3f9cde7
Cvqtko <cvqtkokirilov@gmail.com>
2021-07-05 14:47:57 +0300

    Exercises

--- 02_abstraction_ex/src/ex_02/Main.java
+++ 02_abstraction_ex/src/ex_02/Main.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		System.out.println("Card Ranks:");
+
+		for (CardRanks rank : CardRanks.values()) {
+			System.out.printf("Ordinal value: %d; Name value: %s%n", rank.ordinal(), rank.name());
+		}
 
 	}
 
