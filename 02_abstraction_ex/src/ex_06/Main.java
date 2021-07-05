4784eaf61f60819011061e59d00a58d285f802b0
Cvqtko <cvqtkokirilov@gmail.com>
2021-07-05 15:44:44 +0300

    Exercises

--- 02_abstraction_ex/src/ex_06/Main.java
+++ 02_abstraction_ex/src/ex_06/Main.java
@@ -1,12 +1,28 @@
 package ex_06;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+        long capacity = Long.parseLong(reader.readLine());
+        String[] input = reader.readLine().trim().split("\\s+");
+        Bag bag = new Bag(capacity);
 
-	}
-
+        for (int i = 0; i < input.length; i += 2) {
+            String item = input[i].trim();
+            long quantity = Long.parseLong(input[i + 1].trim());
+            if (item.length() == 3) {
+                bag.addCash(item, quantity);
+            } else if (item.toLowerCase().endsWith("gem")) {
+                bag.addGem(item, quantity);
+            } else if (item.equalsIgnoreCase("gold")) {
+                bag.addGold(quantity);
+            }
+        }
+        System.out.println(bag.ordered());
+    }
 }
