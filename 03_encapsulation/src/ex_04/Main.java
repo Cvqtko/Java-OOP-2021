d76a7f601130e8c5c6a094123bf404c62037d595
Cvqtko <cvqtkokirilov@gmail.com>
2021-07-08 15:06:45 +0300

    Exercises

--- 03_encapsulation/src/ex_04/Main.java
+++ 03_encapsulation/src/ex_04/Main.java
@@ -1,12 +1,14 @@
 package ex_04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
 	}
-
 }
