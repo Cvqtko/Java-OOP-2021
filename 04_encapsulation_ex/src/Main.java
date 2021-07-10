60078a2c03b8349af11b3a9d0d10d6ed95be4758
Cvqtko <cvqtkokirilov@gmail.com>
2021-07-10 17:58:04 +0300

    Exercises

--- 04_encapsulation_ex/src/Main.java
+++ 04_encapsulation_ex/src/Main.java
@@ -1,18 +1,13 @@
-package ex_01;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		double length = Double.parseDouble(reader.readLine());
-		double width = Double.parseDouble(reader.readLine());
-		double height = Double.parseDouble(reader.readLine());
+		Scanner scanner = new Scanner(System.in);
+		double length = Double.parseDouble(scanner.nextLine());
+		double width = Double.parseDouble(scanner.nextLine());
+		double height = Double.parseDouble(scanner.nextLine());
 		StringBuilder sb = new StringBuilder();
 		try {
 			Box box = new Box(length, width, height);
