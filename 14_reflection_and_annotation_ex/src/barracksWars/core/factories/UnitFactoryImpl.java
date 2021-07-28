cf437a0502a216a798171627adb8723dd04a804a
Cvqtko <cvqtkokirilov@gmail.com>
2021-07-28 18:45:00 +0300

    Exercises

--- 14_reflection_and_annotation_ex/src/barracksWars/core/factories/UnitFactoryImpl.java
+++ 14_reflection_and_annotation_ex/src/barracksWars/core/factories/UnitFactoryImpl.java
@@ -2,16 +2,31 @@ package barracksWars.core.factories;
 
 import barracksWars.interfaces.Unit;
 import barracksWars.interfaces.UnitFactory;
+import barracksWars.models.units.Archer;
+import barracksWars.models.units.Gunner;
+import barracksWars.models.units.Horseman;
+import barracksWars.models.units.Pikeman;
+import barracksWars.models.units.Swordsman;
 import jdk.jshell.spi.ExecutionControl;
 
 public class UnitFactoryImpl implements UnitFactory {
 
-	private static final String UNITS_PACKAGE_NAME =
-			"barracksWars.models.units.";
+	private static final String UNITS_PACKAGE_NAME = "barracksWars.models.units.";
 
 	@Override
 	public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException {
-		// TODO: implement for problem 3
-		throw new ExecutionControl.NotImplementedException("message");
+		switch (unitType) {
+		case "Archer":
+			return new Archer();
+		case "Pikeman":
+			return new Pikeman();
+		case "Swordsman":
+			return new Swordsman();
+		case "Horseman":
+			return new Horseman();
+		case "Gunner":
+			return new Gunner();
+		}
+		return null;
 	}
 }
